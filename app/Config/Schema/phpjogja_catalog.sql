b182f471a23d309b8c369e4d295f38bc02150e14
Hiraq <hrxwan@gmail.com>
2013-04-27 08:57:57 +0700

    step 2: pembuatan database

---
+++ app/Config/Schema/phpjogja_catalog.sql
@@ -0,0 +1,74 @@
+-- phpMyAdmin SQL Dump
+-- version 3.4.11.1deb1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Apr 27, 2013 at 08:55 AM
+-- Server version: 5.5.31
+-- PHP Version: 5.4.6-1ubuntu1.2
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `phpjogja_catalog`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `catalog_contacts`
+--
+
+CREATE TABLE IF NOT EXISTS `catalog_contacts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `contact_content` text NOT NULL,
+  `contact_modified` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `catalog_products`
+--
+
+CREATE TABLE IF NOT EXISTS `catalog_products` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `product_name` varchar(255) NOT NULL,
+  `product_desc` text NOT NULL,
+  `product_price` bigint(150) NOT NULL,
+  `product_main_image` varchar(255) NOT NULL,
+  `product_created` datetime NOT NULL,
+  `product_modifie` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `product_name` (`product_name`),
+  KEY `product_price` (`product_price`),
+  FULLTEXT KEY `product_search` (`product_name`,`product_desc`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `catalog_users`
+--
+
+CREATE TABLE IF NOT EXISTS `catalog_users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `username` (`username`,`password`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
